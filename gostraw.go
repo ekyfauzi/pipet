0c4dcfbd2b6f20ebae9fbc4fec299ba7f9ee956b
Eky Fauzi <fauzieky@gmail.com>
2020-09-11 16:46:08 +0700

    setup main function

---
+++ gostraw.go
@@ -0,0 +1,72 @@
+package gostraw
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Straw struct {
+	Driver        string
+	ReadDatabase  *gorm.DB
+	WriteDatabase *gorm.DB
+}
+
+func Init(driver string) *Straw {
+	straw := Straw{
+		Driver: driver,
+	}
+
+	return &straw
+}
+
+func (s *Straw) SetWrite(driver string, host string, port string, database string, user string, password string) {
+	log.Print("initialize write db...")
+	db, err := gorm.Open(s.Driver, fmt.Sprintf("%s:%s@(%s:%s)/%s?parseTime=true&loc=Local", user, password, host, port, database))
+	if err != nil {
+		panic("failed to connect to write database")
+	}
+	fmt.Println("successfuly connected to write db")
+
+	s.WriteDatabase = db
+
+	if s.ReadDatabase == nil {
+		s.ReadDatabase = db
+	}
+}
+
+func (s *Straw) SetRead(driver string, host string, port string, database string, user string, password string) {
+	log.Print("initialize read db...")
+	db, err := gorm.Open(s.Driver, fmt.Sprintf("%s:%s@(%s:%s)/%s?parseTime=true&loc=Local", user, password, host, port, database))
+	if err != nil {
+		panic("failed to connect to read database")
+	}
+	fmt.Println("successfuly connected to read db")
+
+	s.ReadDatabase = db
+
+	if s.WriteDatabase == nil {
+		s.WriteDatabase = db
+	}
+}
+
+func (s *Straw) Where(query interface{}, args ...interface{}) *gorm.DB {
+	return s.ReadDatabase.Where(query, args)
+}
+
+func (s *Straw) Save(value interface{}) *gorm.DB {
+	return s.WriteDatabase.Save(value)
+}
+
+func (s *Straw) Create(value interface{}) *gorm.DB {
+	return s.WriteDatabase.Create(value)
+}
+
+func (s *Straw) Exec(sql string, values ...interface{}) *gorm.DB {
+	return s.WriteDatabase.Exec(sql, values)
+}
+
+func (s *Straw) Conn() *gorm.DB {
+	return s.WriteDatabase
+}
